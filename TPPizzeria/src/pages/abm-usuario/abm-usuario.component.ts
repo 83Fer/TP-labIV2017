e5d39e79c144b779cc4899efe1aa06466d6e9815
Fernando <frivara_mgs@hotmail.com>
2017-06-13 14:24:01 -0300

    Alta baja y modificacion de Usuarios

--- TPPizzeria/src/pages/abm-usuario/abm-usuario.component.ts
+++ TPPizzeria/src/pages/abm-usuario/abm-usuario.component.ts
@@ -24,6 +24,7 @@ export class AbmUsuarioComponent implements OnInit {
   leyenda:string;
 
   //Variables
+  idUsuario: number = 0;
   nomApell : string = "";
   telefono: string = ""; 
   email: string = "";
@@ -35,12 +36,30 @@ export class AbmUsuarioComponent implements OnInit {
   tipo: string = "";
   clave2: string = "";
   // fecIngreso;
+  datosUsuarios : Array<any>;
+  usuario : Array<any>;
+
+  //Filtro
+  filtroTipo: string;
 
   //Modal
-    @ViewChild('autoShownModal') public autoShownModal:ModalDirective;
+    @ViewChild('autoShownModal')
+    public autoShownModal:ModalDirective;
+    public autoShownModal2:ModalDirective;
+    public modificaModal:ModalDirective;
+    public autoShownModal3:ModalDirective;
     public isModalShown:boolean = false;
+    public isModalShown2:boolean = false;
+    public isModalShown3:boolean = false;
+    public isModalModif:boolean = false;
+     
+    
 
-  constructor(public datosApiUsuario : UsuarioService) { }
+  constructor(public datosApiUsuario : UsuarioService) {
+    
+      //this.TraerTodosLosUsuarios();  
+    
+   }
 
   ngOnInit() {
   }
@@ -53,48 +72,156 @@ export class AbmUsuarioComponent implements OnInit {
     }
 
     //Alta de Usuarios
-    AgregarUsuario()
-    {
+    BtnAgregarUsuario(){
+
       if(!this.ValidaCampos())
       {
         this.showModal();
       }
       else
       {
+        try {
 
-        console.log("LLgo aca");
+              console.log("LLgo aca");
 
-        var usuario : Usuario = new Usuario();
+              var usuario : Usuario = new Usuario();
 
-        usuario.nomApell = this.nomApell;
-        usuario.telefono = this.telefono;
-        usuario.email = this.email;
-        usuario.clave = this.clave;
-        usuario.sexo = this.sexo;
-        usuario.fecIngreso = this.FechaIngreso();
-        usuario.foto = this.foto;
-        usuario.direccion = this.direccion;
-        usuario.localidad = this.localidad;
-        usuario.tipo = this.tipo;
+              usuario.nomApell = this.nomApell;
+              usuario.telefono = this.telefono;
+              usuario.email = this.email;
+              usuario.clave = this.clave;
+              usuario.sexo = this.sexo;
+              usuario.fecIngreso = this.FechaIngreso();
+              usuario.foto = "avatar.jpg";
+              usuario.direccion = this.direccion;
+              usuario.localidad = this.localidad;
+              usuario.tipo = this.tipo;
 
-        
-        console.log(usuario);
+              
+              console.log(usuario);
 
-        this.datosApiUsuario.AgregarUsuario(usuario);
+              this.datosApiUsuario.AgregarUsuario(usuario);
 
-        this.showModal();
-        this.titulo = "Alta!!";
-        this.leyenda = "Se ingreso correctamente el Usuario!!";
-        this.Refrescar();
+              this.showModal();
+              this.titulo = "Alta!!";
+              this.leyenda = "Se ingreso correctamente el Usuario!!";
+              this.Refrescar();
+      
+        } catch (error) {
+            this.showModal();
+            this.titulo = "Error!!";
+            this.leyenda = error;
+        }
+
+
 
       }
 
       
     }
 
+    //Actualiza Usuarios
+    BtnActualizar(){
+      if(!this.ValidaCampos())
+      {
+        this.showModal();
+      }
+      else
+      {
+        try {
+              var usuario : Usuario = new Usuario();
+
+              usuario.nomApell = this.nomApell;
+              usuario.telefono = this.telefono;
+              usuario.email = this.email;
+              usuario.clave = this.clave;
+              usuario.sexo = this.sexo;
+              // usuario.fecIngreso = this.FechaIngreso();
+              // usuario.foto = this.foto;
+              usuario.direccion = this.direccion;
+              usuario.localidad = this.localidad;
+              usuario.tipo = this.tipo;
+
+              console.log("El numero es:" + this.idUsuario);
+              this.datosApiUsuario.ActualizaUsuario(this.idUsuario, usuario);
+
+              this.showModal2();
+              this.titulo = "Modifica!!";
+              this.leyenda = "Se actualizo correctamente el Usuario!!";
+              
+              var temp=this;
+              setTimeout(function(){
+                  temp.TraerTodosLosUsuarios();
+              }, 500);  
+              
+      
+        } catch (error) {
+            this.showModal2();
+            this.titulo = "Error!!";
+            this.leyenda = error;
+        }
+      }
+
+    }
+
+    //Baja de Usuario
+    BtnBajaUsuario(){
+      this.datosApiUsuario.BorraUsuario(this.idUsuario);
+
+      var temp=this;
+      setTimeout(function(){
+          temp.TraerTodosLosUsuarios();
+      }, 500);
+      
+    } 
+
+    BorrarUsuario(id:number){
+      this.idUsuario = id;
+      this.showModal3();
+      this.titulo = "Atencion!!";
+      this.leyenda = "Esta seguro que desea dar de baja al usuario !!";
+    }
+
+    //Trae todos los usuarios
+    TraerTodosLosUsuarios(){
+        if(this.filtroTipo == "")
+          this.filtroTipo = "Todos";
+          
+        this.datosApiUsuario.TraerTodosLosUsuarios(this.filtroTipo)
+          .then(datos => {
+          
+            this.datosUsuarios = datos;
+        }).catch( error => {
+          console.log(error);
+        });
+    }
+
+    // Modifica Usuarios
+    ModificarUsuario(id:number){
+
+      this.idUsuario = id;
+      
+      console.log(id);
+        this.datosApiUsuario.TraerUnUsuario(id)
+            .then(datos => {
+            
+              this.usuario = datos;
+          }).catch( error => {
+            console.log(error);
+          });
+          
+          var temp=this;
+          setTimeout(function(){
+              temp.muestraModif();
+              temp.Refrescar();
+              //console.log(user1.nomApell);
+          }, 500);         
+
+    }
+
     //Validacion de campos
-    ValidaCampos():boolean
-    {
+    ValidaCampos():boolean{
+
       this.titulo = "Atencion!!";
       this.leyenda = "Campos requeridos no ingresados";
 
@@ -134,22 +261,62 @@ export class AbmUsuarioComponent implements OnInit {
     }
 
     
-  //Modal function
-    public showModal():void {
-      this.isModalShown = true;
-    }
-  
-    public hideModal():void {
-      this.autoShownModal.hide();
-    }
-  
-    public onHidden():void {
-      this.isModalShown = false;
-    }
-    //**************************** 
+    //Modal function
+      public showModal():void {
+        this.isModalShown = true;
+      }
+    
+      public hideModal():void {
+        this.autoShownModal.hide();
+      }
+    
+      public onHidden():void {
+        this.isModalShown = false;
+      }
+      //Modal function 2
+      public showModal2():void {
+        this.isModalShown2 = true;
+      }
+    
+      public hideModal2():void {
+        this.autoShownModal2.hide();
+      }
+    
+      public onHidden2():void {
+        this.isModalShown2 = false;
+      }
+      //Modal function 3
+      public showModal3():void {
+        this.isModalShown3 = true;
+      }
+    
+      public hideModal3():void {
+        // this.autoShownModal3.hide();
+        this.isModalShown3 = false;
+      }
+    
+      public onHidden3():void {
+        this.isModalShown3 = false;
+      }
+      //**************************** 
+      //Modal function Modifica
+      public muestraModif():void {
+        this.isModalModif = true;
+      }
+    
+      public cierraModal():void {
+        this.modificaModal.hide();
+      }
+    
+      public ocultaModif():void {
+        this.isModalModif = false;
+      }
+      //**************************** 
 
-    FechaIngreso():string
-    {
+    
+  
+    FechaIngreso():string{
+    
       var hoy = new Date();
       var dd = hoy.getDate();
       var mm = hoy.getMonth()+1; 
@@ -173,8 +340,7 @@ export class AbmUsuarioComponent implements OnInit {
       return fecha;
     }
 
-    Refrescar()
-    {
+    Refrescar(){
        //Variables
       this.nomApell = "";
       this.telefono = ""; 
@@ -186,6 +352,24 @@ export class AbmUsuarioComponent implements OnInit {
       this.localidad = "";
       this.tipo = "";
       this.clave2 = "";
+
+      if(this.usuario != null)
+      {
+        this.usuario.forEach(element => {
+          //this.idUsuario = element.id;
+          this.nomApell = element.nomApell;
+          this.telefono = element.telefono; 
+          this.email = element.email;
+          this.clave = element.clave;
+          this.clave2 = element.clave;
+          this.sexo = element.sexo;  
+          this.foto = element.foto; 
+          this.direccion = element.direccion;
+          this.localidad = element.localidad;
+          this.tipo = element.tipo;  
+        });
+        
+      }
     }
 
       
--- TPPizzeria/src/servicios/usuario.service.ts
+++ TPPizzeria/src/servicios/usuario.service.ts
@@ -16,7 +16,7 @@ export class UsuarioService {
 
   TraerTodosLosUsuarios(tipo: string)
   {
-    let url = "http://localhost:8080/AppiPizza/public/index.php/usuaio/" + tipo;    
+    let url = "http://localhost:8080/AppiPizza/public/index.php/usuario/" + tipo;    
     return this.http
       .get(url)
       .toPromise()
@@ -65,6 +65,41 @@ export class UsuarioService {
              .then()
              .catch(this.ErrorExtraerDatos)
 
+  }
+
+  TraerUnUsuario(id: number)
+  {
+    let url = "http://localhost:8080/AppiPizza/public/index.php/usuario/id/" + id;    
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(this.ExtraerDatos)
+      .catch(this.ErrorExtraerDatos);
+  }
+
+  ActualizaUsuario(id:number, usuario: Usuario) 
+  { 
+    
+     let datos={
+       nomApell :  usuario.nomApell ,
+       telefono : usuario.telefono ,
+       email : usuario.email ,
+       clave : usuario.clave ,
+       sexo : usuario.sexo ,
+      //  fecIngreso : usuario.fecIngreso ,
+      //  foto : usuario.foto ,
+       direccion : usuario.direccion,
+       localidad : usuario.localidad,
+      //  estado : "Alta",
+       tipo : usuario.tipo };
+            
+            console.log(datos);
+            console.log(" LLego Usuario");
+    
+      this.http.put("http://localhost:8080/AppiPizza/public/index.php/usuario/" + id , datos)
+             .toPromise()
+             .then()
+             .catch(this.ErrorExtraerDatos)
   }
 
 }
